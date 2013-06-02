4ba2d8c5d33724392ece2d5df352efce8b1e3391
wfwei <cf.wfwei@gmail.com>
2013-06-02 21:43:21 +0800

    final edition

--- src/jp/co/wap/exam/Problem1.java
+++ src/jp/co/wap/exam/Problem1.java
@@ -1,23 +1,51 @@
 package jp.co.wap.exam;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
+
 import jp.co.wap.exam.lib.Interval;
 
 public class Problem1 {
 
+	/* collection.sort edition */
 	public int getMaxIntervalOverlapCount(List<Interval> intervals) {
+		if (intervals == null || intervals.size() == 0) {
+			return 0;
+		}
+		List<TimePoint> timePoints = new ArrayList<TimePoint>();
+		for (Interval interval : intervals) {
+			timePoints.add(new TimePoint(interval.getBeginMinuteUnit(), true));
+			timePoints.add(new TimePoint(interval.getEndMinuteUnit(), false));
+		}
+
+		Collections.sort(timePoints);
+
+		int maxCount = 0, curCount = 0;
+		for (TimePoint tp : timePoints) {
+			if (tp.isStart())
+				curCount++;
+			else
+				curCount--;
+			if (curCount > maxCount)
+				maxCount++;
+		}
+		return maxCount;
+	}
+
+	/* priority queue edition, a little slower */
+	public int getMaxIntervalOverlapCount1(List<Interval> intervals) {
 		if (intervals == null || intervals.size() == 0) {
 			return 0;
 		}
 		int maxCount = 0, curCount = 0;
 		PriorityQueue<TimePoint> timePointQueue = new PriorityQueue<TimePoint>();
 		for (Interval interval : intervals) {
-			// insert sequence is important
-			timePointQueue.add(new TimePoint(interval.getBeginMinuteUnit(),
-					true));
 			timePointQueue
 					.add(new TimePoint(interval.getEndMinuteUnit(), false));
+			timePointQueue.add(new TimePoint(interval.getBeginMinuteUnit(),
+					true));
 		}
 		while (!timePointQueue.isEmpty()) {
 			TimePoint tp = timePointQueue.poll();
@@ -28,7 +56,7 @@ public class Problem1 {
 			if (curCount > maxCount)
 				maxCount++;
 		}
-		return maxCount + 1;
+		return maxCount;
 	}
 
 	private static class TimePoint implements Comparable<TimePoint> {
@@ -46,7 +74,9 @@ public class Problem1 {
 				return 1;
 			else if (this.timeStamp < other.timeStamp)
 				return -1;
-			return 0;
+			else if (!this.start)
+				return 1;
+			return -1;
 		}
 
 		public String toString() {
